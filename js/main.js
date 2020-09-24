aaf61e2b9879f8838e151f10d5938727b46f123c
Nicholas Araújo <njaf@ic.ufal.br>
2020-09-23 21:51:54 -0300

    added board reset, alert on match and error

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let lockBoard = false;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.add("flip");
   if (!hasFlippedCard) {
     hasFlippedCard = true;
@@ -28,15 +29,23 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+  resetBoard();
   alert("ACERTOU!");
 }
 
 function unflipCards() {
+  lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+    resetBoard();
   }, 1500);
   alert("ERROU!");
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
